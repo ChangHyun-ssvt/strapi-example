d2ab7c55b3fe2aa978bded1e215e9f6412c3ef27
ChangHyunKim <changhyun.kim@ssvt.co.kr>
2022-09-16 16:10:37 +0900

    Dockerizing

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env('DATABASE_CLIENT'),
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST'),
+      port: parseInt(env('DATABASE_PORT')),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      // ssl: Boolean(env('DATABASE_SSL')),
+      ssl: false,
     },
-    useNullAsDefault: true,
   },
 });
